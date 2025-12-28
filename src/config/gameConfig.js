70647e09306fbf2b92c6b19ab2f98ea957cfdfa0
rynathusofficialgaming-bit <rynathusofficialgaming@gmail.com>
2025-12-28 03:00:02 +0000

    Update gameConfig.js

--- src/config/gameConfig.js
+++ src/config/gameConfig.js
@@ -16,6 +16,7 @@ export const gameConfig = {
     { time: "23:59", code: "LASTCALL1" },
     { time: "23:59", code: "LASTCALL2" },
     { time: "23:59", code: "TESTDEMO" },
+    { time: "23:59", code: "TESTDEMO1" },
     { time: "ANY", code: "ADMIN" } // Backdoor for testing
   ],
   
@@ -50,4 +51,5 @@ export const gameConfig = {
   }
 
 };
+
 
