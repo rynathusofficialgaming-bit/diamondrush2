1f750a10bc1092f01295934945846fa668ccad53
rynathusofficialgaming-bit <rynathusofficialgaming@gmail.com>
2025-12-28 02:41:36 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import MinesGame from '@/components/MinesGame';
 import { Toaster } from '@/components/ui/toaster';
+import DevToolsBlocker from '@/components/DevToolsBlocker';  // <-- Add this
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <title>Diamond Mines - Gambling Game</title>
-        <meta name="description" content="Play Diamond Mines - Find 3 diamonds to win amazing rewards! Modern sleek gambling game with exciting gameplay." />
-      </Helmet>
-      <MinesGame />
-      <Toaster />
-    </>
+    <DevToolsBlocker>  
+      <>
+        <Helmet>
+          <title>Diamond Mines - Gambling Game</title>
+          <meta name="description" content="Play Diamond Mines - Find 3 diamonds to win amazing rewards! Modern sleek gambling game with exciting gameplay." />
+        </Helmet>
+        <MinesGame />
+        <Toaster />
+      </>
+    </DevToolsBlocker>
   );
 }
 
