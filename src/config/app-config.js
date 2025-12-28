952d74b63463934780e4c3ae4a47b2740188e627
rynathusofficialgaming-bit <rynathusofficialgaming@gmail.com>
2025-12-28 02:43:42 +0000

    Create app-config.js

---
+++ src/config/app-config.js
@@ -0,0 +1,13 @@
+export const APP_CONFIG = {
+
+
+  security: {
+    devtoolsBlocker: {
+      enabled: true,
+      redirectUrl: "https://www.google.com"
+    }
+  },
+  
+
+
+export const API_ENDPOINT = '/api';
